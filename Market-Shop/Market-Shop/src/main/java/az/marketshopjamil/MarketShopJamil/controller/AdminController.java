79fc4a27e49b6bf8975d92f8cf7b373788cf8467
camil1999 <mamedovcamil@gmail.com>
2023-03-04 19:17:07 +0400

    Market-Shop chaining

--- Market-Shop/Market-Shop/src/main/java/az/marketshopjamil/MarketShopJamil/controller/AdminController.java
+++ Market-Shop/Market-Shop/src/main/java/az/marketshopjamil/MarketShopJamil/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,7 +50,10 @@ public class AdminController {
 	public void deleteById(@PathVariable Integer id) {
 		adminService.deleteById(id);
 	}
+	@PostMapping(path = "/login")
+	public void login() {
 
+	}
 	public MappingJacksonValue filter(Object data, String dto, String... fields) {
 		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(fields);
 		FilterProvider provider = new SimpleFilterProvider().addFilter(dto, filter);
